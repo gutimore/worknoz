debf2cc5838e80a57796d294f73af177737d9619
Gonzalo <gutimore@gmail.com>
2024-12-07 22:35:10 -0300

    Add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/worknoz/",
+  base: "/worknoz/", // Ensure this matches your repository name
 });
